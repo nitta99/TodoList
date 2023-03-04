5897b54b4530ead74385fb671cda7ae913836516
nitta99 <93244791+nitta99@users.noreply.github.com>
2023-03-04 11:55:33 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useState } from "react";
+import "./styles.css";
+import { Todo } from "./Todo";
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
+  const onClickData = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
+
+  return (
+    <div className="App">
+      <button onClick={onClickData}>データ表示</button>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
+    </div>
+  );
+}
